7dc197ac8d36bb9f34698a2e2db3417228958d9b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-16 22:00:55 +0100

    Update user_input.py
    act:
    -> problem description added

--- week_1/greeting/user_input.py
+++ week_1/greeting/user_input.py
@@ -2,6 +2,10 @@
 
 from week_1.greeting.greetings import Greeting
 
+# Problem 1: Develop an application that asks the user
+# for her/his name, and then greets them with their
+# name.
+
 def main():
     user_name = input('What is your name? ')
     greeting = Greeting(user_name)
