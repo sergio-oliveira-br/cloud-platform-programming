102386c5c4c46f6bdaa8010dc1d98a7a0d380119
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-18 14:06:19 +0100

    Update calculator.py
    code refactored
    
    act:
    -> the method does not need to be an instance, class has been deleted

--- week_1/recommended_weight/calculator.py
+++ week_1/recommended_weight/calculator.py
@@ -1,17 +1,10 @@
 # week_1/recommended_weight/calculator.py
-from week_1.recommended_weight.user_age import UserAge
-from week_1.recommended_weight.user_height import UserHeight
-
 
 # Problem 2: Write an application that displays the
 # recommended weight given the user’s age and height
 # (in centimeters) using the following formula:
 # RW = (height - 100 + age % 10) * 0.90
 
-class Calculator:
-
-    @staticmethod
-    def weight_calculator(age, height):
-        """Calculates the recommended weight based on age and height."""
-        recommended_weight = (height - 100 + age % 10) * 0.90
-        return recommended_weight
+def weight_calculator(age, height):
+    """Calculates the recommended weight based on age and height."""
+    return (height - 100 + age % 10) * 0.90
