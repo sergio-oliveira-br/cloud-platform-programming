feda8466c458f28bf8c3110ba56180a4c5f65e67
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-19 10:27:57 +0100

    Update main.py
    code refactored
    
    act:
    -> conditionals has been moved from main to service module

--- week_1/recommended_weight/main.py
+++ week_1/recommended_weight/main.py
@@ -26,12 +26,7 @@ def main():
     print(height)
 
     # Calculator
-    if age <= 0 or height > 1000:
-        print("Age and height must be positive numbers. ")
-        return
-
-    else:
-        recommended_w = calculator.weight_calculator(age, height)
+    recommended_w = calculator.weight_calculator(age, height)
 
     print("The recommend height is: " + str(recommended_w)+ "kg")
 
