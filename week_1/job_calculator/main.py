c356208a9023fb281c228d92a5b9e0cac994398c
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-09-19 16:39:22 +0100

    Create main.py

---
+++ week_1/job_calculator/main.py
@@ -0,0 +1,22 @@
+
+
+# Problem 4: Having just started college, Bob has been busy
+# looking for a part-time job to fund his new college social life
+# and after only two weeks of looking he has managed to get
+# two job offers! Each job comes with different hours, basic rates
+# of pay and over-time rates so he needs to work out which
+# would get him the most money. Develop an application that
+# would allow Bob to enter his basic pay rate, his number of
+# regular hours work per week and his number of overtime
+# hours per week. The application should then calculate and
+# display Bob’s total basic pay for the week, his overtime pay for
+# the week and his total pay including overtime. Note: The
+# overtime rate is 1.5 times the basic rate of pay.
+
+
+def main():
+    print("Problem 4 - Job Calculator")
+
+
+if __name__ == '__main__':
+    main()
